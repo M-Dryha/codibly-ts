4ad89424e733a2d90e7ab3f781b0963a83841c1a
Mariia Dryha <m.pishchanska@gmail.com>
2023-02-13 12:42:26 +0200

    fix:change container

--- src/App.tsx
+++ src/App.tsx
@@ -4,21 +4,10 @@ import Container from '@mui/material/Container';
 import Loader from './components/Loader';
 const BasicView = lazy(() => import('./Views/BasicView/BasicView'));
 
-const styles = {
-  Container: {
-    background: 'linear-gradient(90deg, #69b7eb, #b3dbd3, #f4d6db)',
-    backgroundSize: 'cover',
-    backgroundRepeat: 'no-repeat',
-    backgroundPosition: 'center',
-    height: '100vh',
-    pt: '20px',
-  },
-};
-
 export const App: React.FC = () => {
   return (
     <Suspense fallback={<Loader />}>
-      <Container maxWidth="xl" sx={{ pt: 4, pb: 4 }} style={styles.Container}>
+      <Container maxWidth="xl" sx={{ pt: 4, pb: 4 }}>
         <Routes>
           <Route path="/" element={<BasicView />} />
         </Routes>
